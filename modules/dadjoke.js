09700bde8476132c9cb786671a469a59a7255028
Dave Hendler <sollaires@gmail.com>
2019-12-15 20:02:36 -0800

    dadjoke: Ignore bot messages

--- modules/dadjoke.js
+++ modules/dadjoke.js
@@ -7,7 +7,12 @@ export default (client) => {
   const url = 'https://icanhazdadjoke.com/';
 
   client.on('message', async message => {
-    if (message.content.match(/^dad ?joke$/)) { 
+    // ignore bot messages
+    if (message.author.bot) {
+      return;
+    }
+    if (message.content.match(/^dad ?joke$/)) {
+      console.log('Looking up a dad joke');
       const response = await got(url, { headers: { Accept: 'application/json' } });
 
       const results = JSON.parse(response.body);
