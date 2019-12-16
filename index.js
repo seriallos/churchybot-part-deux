b74f8ff759096552035d06d6e4af2c1d00aca5a7
Dave Hendler <sollaires@gmail.com>
2019-12-15 16:13:07 -0800

    Setup babel, initial discord connection

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Churchybot is go');
+import Discord from 'discord.js';
+
+console.log('ChurchyBot starting');
+
+async function main() {
+  const client = new Discord.Client();
+
+  const TOKEN = process.env.CHURCHYBOT_DISCORD_TOKEN;
+
+  client.on('ready', () => console.log('Discord client ready'));
+  client.on('error', error => console.error('Discord client error', error));
+  client.on('warn', warning => console.error('Discord client warning', warning));
+  client.on('reconnecting', () => console.log('Discord client reconnecting'));
+  client.on('reconnecting', () => console.log('Discord client reconnecting'));
+
+  client.login(TOKEN);
+
+  client.on('message', message => {
+    if (message.content === 'ping') {
+      message.channel.send('pong');
+    }
+  });
+}
+
+main();
