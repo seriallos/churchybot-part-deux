32377a7285b039a667d3e39fa9b20d7045ab3117
Dave Hendler <sollaires@gmail.com>
2020-01-04 16:41:31 -0800

    imageme: Take a stab at a collpase/imageme shift command to let folks shift imagemes to #image-me-roulette

--- modules/imageme.js
+++ modules/imageme.js
@@ -2,12 +2,20 @@ import Discord from 'discord.js';
 import _ from 'lodash';
 import got from 'got';
 
+import { getChurchybotCommand } from '../util';
+
 const SPOILER_CHANNELS = [
 ];
+
+const COLLAPSE_SHIFT_CHANNEL = 'image-me-roulette';
 
 export default (client) => {
   const CSE_ID = process.env.CHURCHYBOT_GOOGLE_CSE_ID;
   const CSE_KEY = process.env.CHURCHYBOT_GOOGLE_CSE_KEY;
+
+  const lastChannelImageMessage  = {};
+
+  const collapseChannel = guild.channels.find(c => c.name === COLLAPSE_SHIFT_CHANNEL);
 
   const search = async (text, animated) => {
     const start = 1 + (10 * _.random(0, 9));
@@ -45,11 +53,16 @@ export default (client) => {
       if (message.author.bot) {
         return;
       }
+
+      const channelName = _.toLower(message.channel.name);
+
       let matches;
       let searchText;
       let numImages = 1;
       let animated = false;
-      let spoiler = _.includes(SPOILER_CHANNELS, _.toLower(message.channel.name));
+      let canCollapse = channelName !== COLLAPSE_SHIFT_CHANNEL;
+      let spoiler = _.includes(SPOILER_CHANNELS, channelName);
+
       if (matches = message.content.match(/^(spoiler )?image me (.+)/i)) {
         spoiler = spoiler || Boolean(matches[1]);
         searchText = matches[2];
@@ -64,20 +77,42 @@ export default (client) => {
         };
         searchText = searchMap[matches[1]] || matches[1];
         numImages = 5;
+        // do not track / allow collapse on bombs
+        canCollapse = false;
+      } else if (getChurchybotCommand(message) === 'collapse') {
+        const lastMessage = lastChannelImageMessage[channelName];
+        if (channelName === COLLAPSE_SHIFT_CHANNEL) {
+          message.channel.send('Cannot collapse in this channel');
+        } else if (lastMessage) {
+          console.log(`imageme: ${channelName}: Collapsed last image`);
+          await collapseChannel.send(`Image collapse requested from ${channelName}:`);
+          await collapseChannel.send(lastMessage.content);
+          lastMessage.edit(`Collapse requested. Image moved to #${COLLAPSE_SHIFT_CHANNEL}`);
+        } else {
+          console.log(`imageme: ${channelName}: Nothing in MRU image channel history`);
+          message.channel.send('Nothing to collapse (most recent already collapsed or nothing in memory)');
+        }
       }
 
       if (searchText) {
-        console.log(`imageme: Searching for "${searchText}", animated: ${animated}, numImages: ${numImages}`);
+        console.log(
+          `imageme: ${channelName}: Searching for "${searchText}", animated: ${animated}, numImages: ${numImages}`,
+        );
         const results = await search(searchText, animated);
         _.each(_.sampleSize(results, numImages), imageUrl => {
+          let sentMessage;
           if (spoiler) {
-            message.channel.send(`|| ${imageUrl} ||`);
+            sentMessage = await message.channel.send(`|| ${imageUrl} ||`);
           } else {
             const embed = new Discord.RichEmbed()
               .setTitle(searchText)
               .setURL(imageUrl)
               .setImage(imageUrl);
-            message.channel.send(embed);
+            sentMessage = await message.channel.send(embed);
+          }
+
+          if (canCollapse) {
+            lastChannelImageMessage[channelName] = sentMessage;
           }
         });
       }
