20e2c436d8498d857bacb4224b437a22969e944f
Jason Winget <jwinget@gmail.com>
2020-01-05 09:25:29 -0500

    Added seed text and random chattiness feature

--- modules/suckbot.js
+++ modules/suckbot.js
@@ -1,7 +1,11 @@
 import Discord from 'discord.js';
 import got from 'got';
+import _ from 'lodash';
 
 import { getChurchybotCommand } from '../util';
+
+// Control how often the bot spouts off at random
+const CHATTINESS = 1;
 
 export default (client) => {
   const PREFIXES = ['churchybot', 'suckbot'];
@@ -13,16 +17,34 @@ export default (client) => {
     if (message.author.bot) {
       return;
     }
-    if (getChurchybotCommand(message).match(/^talk to me$/)) {
+    
+    let seedText = "well, actually";
+    
+    if (matches = getChurchybotCommand(message).match(/^talk to me ?(about)(.+)/)) {
+      // Change seedText to anything after "about"
+      seedText = matches.slice(-1)[0];
       console.log('sentence requested');
       message.channel.startTyping();
-      const response = await got.post(url, {json: {length: 45, nsamples: 1, temperature: 0.75 }});
+      const response = await got.post(url, {json: {length: 45, nsamples: 1, temperature: 0.75, prefix: seedText }});
       const results = JSON.parse(response.body);
 
       const text = results.text;
 
       message.channel.send(text);
       message.channel.stopTyping();
+    } else {
+      const roll = 100 * _.random(0, 1, true);
+      if (roll < CHATTINESS) {
+        message.channel.startTyping();
+        // Use the last couple of words of the message as the text generation seed
+        seedText = message.content.split(" ").splice(-2).join(" ")
+        
+        const response = await got.post(url, {json: {length: 45, nsamples: 1, temperature: 0.75, prefix: seedText}});
+        const results = JSON.parse(response.body);
+        const text = results.text;
+        
+        message.channel.send(text);
+        message.channel.stopTyping();
     }
   });
 }
