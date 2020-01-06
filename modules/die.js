7b3c9fd1a3f9ffa36c380ef37264bc6b8c3eb4ac
Dave Hendler <sollaires@gmail.com>
2020-01-06 12:00:54 -0800

    die: Just die immediately after verifying the message has sent

--- modules/die.js
+++ modules/die.js
@@ -1,12 +1,12 @@
 import { getChurchybotCommand } from '../util';
 
 export default client => {
-  client.on('message', message => {
+  client.on('message', async message => {
     const command = getChurchybotCommand(message);
     if (command === 'die') {
       console.log('die requested');
-      message.channel.send('Restarting bot...');
-      setTimeout(() => process.exit(0), 1 * 1000);
+      await message.channel.send('Restarting bot...');
+      process.exit(0);
     }
   });
 }
