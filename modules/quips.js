7f5c54987209c77a8a66131436dd52f11102cea2
Dave Hendler <sollaires@gmail.com>
2020-07-13 18:23:42 -0700

    quips: Add the rest

--- modules/quips.js
+++ modules/quips.js
@@ -3,10 +3,63 @@ import _ from 'lodash';
 
 const triggers = [{
   listen: /(tv mount|mission accomplished|job well done)/i,
-  response: 'https://i.imgur.com/ZidtWtL.jpg',
+  image: 'https://i.imgur.com/ZidtWtL.jpg',
 }, {
   listen: /YACHTS/,
-  response: 'https://i.imgur.com/Cln9dqs.png',
+  image: 'https://i.imgur.com/Cln9dqs.png',
+}, {
+  listen: /shocked$/i,
+  image: 'https://media.giphy.com/media/AaQYP9zh24UFi/giphy.gif',
+}, {
+  listen: /(fry not sure|^not sure if)/i,
+  image: 'http://giphygifs.s3.amazonaws.com/media/ANbD1CCdA3iI8/giphy.gif',
+}, {
+  listen: /tapping/,
+  image: 'https://media.giphy.com/media/d3mlE7uhX8KFgEmY/giphy.gif',
+}, {
+  listen: /(money crying|wiping tears)/i,
+  image: 'http://giphygifs.s3.amazonaws.com/media/94EQmVHkveNck/giphy.gif',
+}, {
+  listen: /my fetish/i,
+  image: [
+    'http://giphygifs.s3.amazonaws.com/media/JxFLe8raHIYhy/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/41io4H3B7wSti/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/2sWVvF0wMkIyQ/giphy.gif',
+    'https://media.giphy.com/media/u6MSNuAAIMaWc/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/IPgIu0v6z77c4/giphy.gif'
+  ],
+}, {
+  listen: /(fillion|mal) (facepalm|confused|speechless)/i,
+  image: 'https://giphy.com/gifs/story-conversation-topic-vUEznRmVQfG2Q',
+}, {
+  listen: /take my money/i,
+  image: 'http://i.imgur.com/QlmfC.jpg',
+}, {
+  listen: /wrong\./i,
+  image: 'https://giphy.com/gifs/ceeN6U57leAhi',
+}, {
+  listen: /what did you think/i,
+  image: 'https://media.giphy.com/media/YP6GvHrK2bo1q/giphy.gif',
+}, {
+  listen: /rimshot/,
+  image: [
+    'http://giphygifs.s3.amazonaws.com/media/SUeUCn53naadO/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/1gArwncRlXac8GIhNy8/giphy.gif',
+    'https://media.giphy.com/media/AR0MThYLSnmGQ/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/ItAmGFb0uiZz2/giphy.gif',
+    'https://media.giphy.com/media/fWgPzLGeI4okiaG8QT/giphy.gif',
+    'https://media.giphy.com/media/3oFzmhMIcKK846ku5i/giphy.gif',
+    'http://giphygifs.s3.amazonaws.com/media/gKYpJXuvVeryo/giphy.gif'
+  ],
+}, {
+  listen: /ChurchyBot are you alive/i,
+  image: 'https://giphy.com/gifs/game-of-thrones-got-arya-stark-9RKLlD2oz5c7m',
+}, {
+  listen: /turbine/i,
+  image: 'POWERED BY OUR FANS',
+}, {
+  listen: /so say we all/i,
+  image: 'https://media.giphy.com/media/NM4E1FcXQK6oE/giphy.gif',
 }];
 
 const makeEmbed = (imageUrl) => {
@@ -17,7 +70,7 @@ const makeEmbed = (imageUrl) => {
 
 export default (client) => {
   client.on('message', async message => {
-    _.each(triggers, ({ listen, response }) => {
+    _.each(triggers, ({ listen, response, image }) => {
       let match = false;
       if (_.isRegExp(listen) && listen.test(message.content)) {
         match = true;
@@ -26,13 +79,23 @@ export default (client) => {
       }
       if (match) {
         let msg;
-        if (_.isArray(response)) {
-          msg = _.sample(response);
-        } else {
-          msg = response;
+        if (image) {
+          if (_.isArray(image)) {
+            msg = _.sample(image);
+          } else {
+            msg = image;
+          }
+          const embed = makeEmbed(msg);
+          message.channel.send(embed);
+        } else if (message) {
+          let msg;
+          if (_.isArray(message)) {
+            msg = _.sample(message);
+          } else {
+            msg = message;
+          }
+          message.channel.send(msg);
         }
-        const embed = makeEmbed(response);
-        message.channel.send(embed);
         return false;
       }
       return true;
